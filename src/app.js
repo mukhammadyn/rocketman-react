590d7a68a1b3b8514fcb12867f753c218e92fc08
Sanjarbek7777 <rsanjarbek450@gmail.com>
2021-11-04 10:49:02 +0500

    Add buttons

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
+import Buttons from "./components/buttons/buttons";
 import HeaderDropdown from "./components/header-dropdown/header-dropdown";
 import LoginPage from "./components/login-page/login-page";
 
 function App() {
   return (
     <>
+
     </>
   );
 }
